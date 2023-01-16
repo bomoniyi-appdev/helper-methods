ce873305fb99305e54a4add055d130292cd55216
bomoniyi <bomoniyi@chicagobooth.edu>
2023-01-16 21:53:32 +0000

    Refractor_11 Refractor_11

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Rails.application.routes.draw do
-  get "/" => "movies#index" 
+  
+  root "movies#index" 
 
   # Routes for the Movie resource:
 
   # CREATE
-  post "/movies" => "movies#create"
-  get "/movies/new" => "movies#new"
+  post "/movies" => "movies#create", as: :movies  #movies_url and movies_path
+  get "/movies/new" => "movies#new", as: :new_movie  #new_movie_url and new_movie_path
           
   # READ
   get "/movies" => "movies#index"
   get "/movies/:id" => "movies#show"
   
   # UPDATE
-  patch "/movies/:id" => "movies#update"
-  get "/movies/:id/edit" => "movies#edit"
+  patch "/movies/:id" => "movies#update", as: :movie  #movie_path() (expects and argument to populate the :id segment)
+  get "/movies/:id/edit" => "movies#edit", as: :edit_movie #edit_movie_path() (expects and argument to populate the :id segment)
   
   # DELETE
   delete "/movies/:id" => "movies#destroy"
